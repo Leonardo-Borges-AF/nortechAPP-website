a63f01e582e9cfdd1760b1723e319318a20e5c66
Leonardo Borges <leoborgesaf@hotmail.com>
2024-07-25 18:09:23 -0300

    fix-hover

--- src/components/header/Header.components.tsx
+++ src/components/header/Header.components.tsx
@@ -2,14 +2,16 @@ import LogoNortechApp from "/images/logo-nortechapp.png";
 import emails from "/images/icons/emails.png";
 import { LuMenu } from "react-icons/lu";
 
+import "./header.css";
+
 export const Header = () => {
   
   return(
-    <div className="px-28 py-[18px] flex justify-between bg-white bg-opacity-[0.04] border-b border-b-white border-opacity-[0.08]">
+    <div className="md:px-28 px-8 py-[18px]  flex justify-between bg-white bg-opacity-[0.04] border-b border-b-white border-opacity-[0.08]">
       <div className="w-[650px] flex gap-6 items-center">
         <img src={LogoNortechApp} alt="logo NortechAPP" className="h-9 w-40"/>
-        <div className="flex border w-[22px] bg-white opacity-20 rotate-90 h-[1px]"/>
-        <div className="flex gap-2">
+        <div className="hidden md:flex border w-[22px] bg-white opacity-20 rotate-90 h-[1px]"/>
+        <div className="hidden md:flex gap-2">
           <h3 className=" text-white text-base">
             More
           </h3>
@@ -21,10 +23,14 @@ export const Header = () => {
       </div>
 
       <div className="flex gap-6 items-center">
-        <button className="flex gap-[10px] py-[15px] bg-[#9841FF] text-white border-4 rounded-lg border-solid border-[#9841FF] border-opacity-25 " 
+        <button
+        className="hidden md:flex gap-[10px] py-4 bg-[#9841FF]  border-4 rounded-lg border-solid border-[#9841FF] border-opacity-25 group hover:border-white animate-button " 
         style={{ boxShadow: "0px 8px 24px 0px #AE6BFF40" }}>
-          <img src={emails} className="size-6"/>
-          Join the whitelist
+          <img src={emails} className="size-6 group-hover:filter group-hover:brightness-0 "/>
+          <p className="text-white group-hover:text-black z-10">
+            Join the whitelist
+          </p>
+          
         </button>
         <LuMenu className="text-white size-6"/>
       </div>
