23939b41d3744f5ca9cf19a5cd1c462f5052e5a3
Leonardo Borges <leoborgesaf@hotmail.com>
2024-08-15 20:58:53 -0300

    a

--- src/pages/Home/components/Session2.component.tsx
+++ src/pages/Home/components/Session2.component.tsx
@@ -45,7 +45,7 @@ export const Session2 = () => {
   const aboutStyle = selected === 1 ? 'text-[#6001D1]' : selected === 2 ? 'text-[#6001D1]' : 'text-[#fff]'
 
   return (
-    <div id="session-2" className='bg-white relative overflow-hidden'>
+    <div id="session-2" className='bg-[#FFFFFF] relative overflow-hidden'>
       <div className="pt-20 md:pb-32 flex gap-3 transition-all duration-300 relative max-w-[1184px] mx-auto flex-col md:flex-row">
         <div id="background-image" className="absolute top-[200px] left-[-800px] w-full h-full bg-cover bg-center transition-all duration-700 selectDisable" style={{ transform: `rotate(${rotation}deg)` }}>
           <img id="background-image" src={Background} className='absolute opacity-15 top-[-900px] left-[-200px] rotate-10 w-[3322px] h-[3147px] selectDisable -z-50' />
